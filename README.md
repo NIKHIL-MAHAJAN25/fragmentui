5b7a084c47c5428a7abf3931208cfffe4438e310
Nikhil mahajan <nikhilmahajan8787@gmail.com>
2025-03-02 15:59:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fragmentui
+ gympage
---
+++ app/src/main/java/com/nikhil/fragmentui/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.nikhil.fragmentui
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        var button: Button?=null
+        var button2:Button?=null
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
