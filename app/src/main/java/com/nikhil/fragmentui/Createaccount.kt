a236c38551c06339e115cc97ef579be208b33a22
Nikhil mahajan <nikhilmahajan8787@gmail.com>
2025-03-04 15:58:47 +0530

    finalayout

--- app/src/main/java/com/nikhil/fragmentui/Createaccount.kt
+++ app/src/main/java/com/nikhil/fragmentui/Createaccount.kt
@@ -1,38 +1,20 @@
 package com.nikhil.fragmentui
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
-class MainActivity : AppCompatActivity() {
+class Createaccount : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        var button: Button?=null
-        var button2:Button?=null
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_createaccount)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        button=findViewById(R.id.btnsign)
-        button2=findViewById(R.id.btnsignin)
-        button?.setOnClickListener()
-        {
-            var intent=Intent(this,signinpage::class.java)
-            startActivity(intent)
-
-        }
-        button2?.setOnClickListener()
-        {
-            var intent1=Intent(this,Createaccount::class.java)
-            startActivity(intent1)
         }
     }
 }
